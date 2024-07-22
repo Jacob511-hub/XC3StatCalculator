ec7d07f518323292a1dfde73b85d58cb6d10ac99
Jacob511-hub <jacob511@outlook.com>
2024-07-22 13:59:41 -0230

    Class and Weapon stat modifiers added for remaining classes

--- src/classes.js
+++ src/classes.js
@@ -252,3 +252,326 @@ const statsLoneExile = {
         "block": 30,
     }
 }
+
+const statsIncursor = {
+    "class_stats": {
+        "hp": 0.75,
+        "attack": 1.20,
+        "healing_power": 0.20,
+        "dexterity": 1.05,
+        "agility": 1.00,
+        "physical_defense": 20,
+        "ether_defense": 10,
+    },
+    "weapon_stats": {
+        "attack": 575,
+        "critical": 35,
+        "block": 6,
+    }
+}
+
+const statsLostVanguard = {
+    "class_stats": {
+        "hp": 1.25,
+        "attack": 0.80,
+        "healing_power": 0.35,
+        "dexterity": 1.20,
+        "agility": 0.90,
+        "physical_defense": 50,
+        "ether_defense": 35,
+    },
+    "weapon_stats": {
+        "attack": 519,
+        "critical": 20,
+        "block": 45,
+    }
+}
+
+const statsSignifer = {
+    "class_stats": {
+        "hp": 0.60,
+        "attack": 0.85,
+        "healing_power": 0.75,
+        "dexterity": 1.05,
+        "agility": 0.90,
+        "physical_defense": 10,
+        "ether_defense": 30,
+    },
+    "weapon_stats": {
+        "attack": 514,
+        "critical": 16,
+        "block": 16,
+    }
+}
+
+const statsSoulhackerPower = {
+    "class_stats": {
+        "hp": 0.75,
+        "attack": 1.30,
+        "healing_power": 0.15,
+        "dexterity": 1.05,
+        "agility": 0.75,
+        "physical_defense": 20,
+        "ether_defense": 10,
+    },
+    "weapon_stats": {
+        "attack": 580,
+        "critical": 15,
+        "block": 25,
+    }
+}
+
+const statsSoulhackerStrike = {
+    "class_stats": {
+        "hp": 0.75,
+        "attack": 1.15,
+        "healing_power": 0.30,
+        "dexterity": 1.20,
+        "agility": 0.95,
+        "physical_defense": 15,
+        "ether_defense": 15,
+    },
+    "weapon_stats": {
+        "attack": 580,
+        "critical": 15,
+        "block": 25,
+    }
+}
+
+const statsSoulhackerTough = {
+    "class_stats": {
+        "hp": 1.25,
+        "attack": 0.95,
+        "healing_power": 0.15,
+        "dexterity": 0.95,
+        "agility": 0.65,
+        "physical_defense": 45,
+        "ether_defense": 35,
+    },
+    "weapon_stats": {
+        "attack": 580,
+        "critical": 15,
+        "block": 25,
+    }
+}
+
+const statsSoulhackerDodge = {
+    "class_stats": {
+        "hp": 1.10,
+        "attack": 0.95,
+        "healing_power": 0.15,
+        "dexterity": 1.05,
+        "agility": 1.20,
+        "physical_defense": 35,
+        "ether_defense": 45,
+    },
+    "weapon_stats": {
+        "attack": 580,
+        "critical": 15,
+        "block": 25,
+    }
+}
+
+const statsSoulhackerHeal = {
+    "class_stats": {
+        "hp": 0.70,
+        "attack": 0.75,
+        "healing_power": 1.00,
+        "dexterity": 1.10,
+        "agility": 0.9,
+        "physical_defense": 10,
+        "ether_defense": 30,
+    },
+    "weapon_stats": {
+        "attack": 580,
+        "critical": 15,
+        "block": 25,
+    }
+}
+
+const statsSoulhackerBalanced = {
+    "class_stats": {
+        "hp": 0.65,
+        "attack": 0.90,
+        "healing_power": 0.75,
+        "dexterity": 1.05,
+        "agility": 0.90,
+        "physical_defense": 10,
+        "ether_defense": 30,
+    },
+    "weapon_stats": {
+        "attack": 580,
+        "critical": 15,
+        "block": 25,
+    }
+}
+
+const statsMartialArtist = {
+    "class_stats": {
+        "hp": 0.75,
+        "attack": 1.10,
+        "healing_power": 0.25,
+        "dexterity": 1.20,
+        "agility": 1.20,
+        "physical_defense": 15,
+        "ether_defense": 15,
+    },
+    "weapon_stats": {
+        "attack": 554,
+        "critical": 36,
+        "block": 16,
+    }
+}
+
+const statsTroubadour = {
+    "class_stats": {
+        "hp": 0.55,
+        "attack": 0.60,
+        "healing_power": 1.15,
+        "dexterity": 1.10,
+        "agility": 1.00,
+        "physical_defense": 10,
+        "ether_defense": 30,
+    },
+    "weapon_stats": {
+        "attack": 432,
+        "critical": 16,
+        "block": 8,
+    }
+}
+
+const statsSeraph = {
+    "class_stats": {
+        "hp": 0.80,
+        "attack": 1.30,
+        "healing_power": 0.15,
+        "dexterity": 1.00,
+        "agility": 0.85,
+        "physical_defense": 20,
+        "ether_defense": 10,
+    },
+    "weapon_stats": {
+        "attack": 661,
+        "critical": 25,
+        "block": 10,
+    }
+}
+
+const statsMachineAssassin = {
+    "class_stats": {
+        "hp": 0.80,
+        "attack": 1.25,
+        "healing_power": 0.25,
+        "dexterity": 1.20,
+        "agility": 0.75,
+        "physical_defense": 20,
+        "ether_defense": 10,
+    },
+    "weapon_stats": {
+        "attack": 635,
+        "critical": 30,
+        "block": 20,
+    }
+}
+
+const statsLifesage = {
+    "class_stats": {
+        "hp": 0.65,
+        "attack": 0.75,
+        "healing_power": 1.30,
+        "dexterity": 1.20,
+        "agility": 1.10,
+        "physical_defense": 15,
+        "ether_defense": 25,
+    },
+    "weapon_stats": {
+        "attack": 559,
+        "critical": 20,
+        "block": 30,
+    }
+}
+
+const statsRoyalSummoner = {
+    "class_stats": {
+        "hp": 0.65,
+        "attack": 1.30,
+        "healing_power": 0.60,
+        "dexterity": 1.30,
+        "agility": 1.10,
+        "physical_defense": 15,
+        "ether_defense": 15,
+    },
+    "weapon_stats": {
+        "attack": 671,
+        "critical": 30,
+        "block": 10,
+    }
+}
+
+const statsNoponicChampion = {
+    "class_stats": {
+        "hp": 1.20,
+        "attack": 0.95,
+        "healing_power": 0.0,
+        "dexterity": 1.20,
+        "agility": 1.30,
+        "physical_defense": 45,
+        "ether_defense": 40,
+    },
+    "weapon_stats": {
+        "attack": 519,
+        "critical": 15,
+        "block": 35,
+    }
+}
+
+const statsLapidarist = {
+    "class_stats": {
+        "hp": 0.55,
+        "attack": 0.80,
+        "healing_power": 1.20,
+        "dexterity": 1.15,
+        "agility": 1.10,
+        "physical_defense": 10,
+        "ether_defense": 15,
+    },
+    "weapon_stats": {
+        "attack": 549,
+        "critical": 35,
+        "block": 6,
+    }
+}
+
+const statsGrandMarshal = {
+    "class_stats": {
+        "hp": 1.10,
+        "attack": 0.95,
+        "healing_power": 0.60,
+        "dexterity": 1.10,
+        "agility": 1.25,
+        "physical_defense": 40,
+        "ether_defense": 40,
+    },
+    "weapon_stats": {
+        "attack": 559,
+        "critical": 16,
+        "block": 30,
+    }
+}
+
+const statsMasterDriver = {
+    "class_stats": {
+        "hp": 0.75,
+        "attack": 0.50,
+        "healing_power": 0.50,
+        "dexterity": 1.30,
+        "agility": 1.15,
+        "physical_defense": 15,
+        "ether_defense": 15,
+    },
+    "weapon_stats": {
+        "attack": 686,
+        "critical": 32,
+        "block": 8,
+    }
+}
